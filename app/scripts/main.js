358fe310968a0be89b23f0ed06bec9a23c5cac57
Josh Kasuboski <kasuboski@wisc.edu>
2016-05-11 19:11:09 -0500

    Have up on hover working for bottom cards

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  //animate bottom cards on hover
+  $(".cards-container > ul > li > .card").hover(function() {
+    $(this).stop().animate({
+        top: -25
+    }, 200, "linear");
+  }, function() {
+      $(this).stop().animate({
+          top: 0
+      }, 200, "linear");
+  });
+});
