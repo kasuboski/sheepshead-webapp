759672f6deca4036d28a21265026eadf99d3fc7b
Josh Kasuboski <kasuboski@wisc.edu>
2016-05-15 16:55:05 -0500

    have the ability to play a card

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,12 +9,13 @@ $(function() {
     }
   });
 
-  players[0].hand.forEach(card => {
-    addCardtoUI(card);
+  players[0].hand.forEach(function(card, index, array) {
+    addCardtoUI(card, index);
   });
 
-    //animate bottom cards on hover
-  $(".cards-container > ul > li > .card").hover(function() {
+  //animate bottom cards on hover
+  let bottom_cards = $(".cards-container > ul > li > .card");
+  bottom_cards.hover(function() {
     $(this).stop().animate({
         top: -25
     }, 200, "linear");
@@ -23,14 +24,28 @@ $(function() {
           top: 0
       }, 200, "linear");
   });
+
+  //add click listeners to bottom cards
+  bottom_cards.click(function() {
+    //remove card from view
+    let card = $(this);
+    card.parent().remove();
+
+    //add card to played card spot remove old one
+    $("#player-played-card").empty();
+    //reset style
+    card.removeAttr("style");
+    $("#player-played-card").append(card);
+  });
+
 });
 
-let addCardtoUI = function(card) {
+let addCardtoUI = function(card, index) {
   let player_card_container = $(".cards-container > ul");
 
   //create card and append to card container div
   let new_li = $("<li>");
-  let img = $("<img>", {src:card.image, class:"card"});
+  let img = $("<img>", {src:card.image, class:"card", 'data-index': index});
 
   new_li.append(img);
   player_card_container.append(new_li);
